79a476971485084fe5461327e06ef1d6eaf90f43
Manrojw <manevichrr@yandex.ru>
2023-10-08 16:23:35 -0400

    Finished FelineTest Edited dependencies for JaCoCo

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,33 +1,41 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
     @Mock
-    private Feline feline;
+    private Feline mockFeline;
+
+    @Before
+    public void setUp() {
+        mockFeline = Mockito.mock(Feline.class);
+    }
 
     @Test
     public void testGetKittens() {
-        when(feline.getKittens()).thenReturn(1);
-        int kittens = feline.getKittens();
-        assertEquals(1, kittens);
+        when(mockFeline.getKittens()).thenReturn(2);
+
+        int kittens = mockFeline.getKittens();
+        assertEquals(2, kittens);
     }
 
     @Test
     public void testEatMeat() throws Exception {
-        when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
-        List<String> actualFood = feline.eatMeat();
+        when(mockFeline.eatMeat()).thenReturn(List.of("Животные", "Птицы"));
+
+        List<String> expectedFood = List.of("Животные", "Птицы");
+        List<String> actualFood = mockFeline.eatMeat();
         assertEquals(expectedFood, actualFood);
     }
 }
