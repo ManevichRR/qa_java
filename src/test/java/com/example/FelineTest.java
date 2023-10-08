a1ff6bf8d3c8b5f217353b128f4ab753f4f4cf4e
Manrojw <manevichrr@yandex.ru>
2023-10-08 14:36:32 -0400

    Переписал без использования org.mockito.MockitoAnnotations; Тк мы его не использовали в обучении

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,42 +1,31 @@
 package com.example;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
 import java.util.List;
-import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class) //Забыл про Runner
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
     @Mock
     private Feline feline;
 
-    @Before
-    public void setUp() {
-        feline = mock(Feline.class); // Создаем мок
-    }
-
     @Test
     public void testGetKittens() {
-
-        when(feline.getKittens()).thenReturn(1); // Условия для мока
-
-        // Вызываем метод и проверяем результат
+        when(feline.getKittens()).thenReturn(1);
         int kittens = feline.getKittens();
         assertEquals(1, kittens);
     }
 
     @Test
     public void testEatMeat() throws Exception {
-
-        when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));  // Условия для мока
-
-        // Вызываем метод и проверяем результат
+        when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         List<String> actualFood = feline.eatMeat();
         assertEquals(expectedFood, actualFood);
