a2643c3189da5efd75fe225846bf78cc92d9b45b
Manrojw <manevichrr@yandex.ru>
2023-10-08 15:33:52 -0400

    LionTest done. Edited constructor for Lion class

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -8,7 +8,8 @@ public class Lion {
     Predator predator; //Убираем зависимость от Филайн (я на федоре страдаю, не нашел где в гноме раскладку менять)
     //Editing cz texted wrong comment for last commit. Right comment for prev commit is: 'DI done'
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Predator predator) throws Exception {
+        this.predator=predator;  //Отредачил конструктор, забыл добавить предатора
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -17,6 +18,7 @@ public class Lion {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
+
 
     public int getKittens() {
         return predator.getKittens();
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,4 +1,47 @@
 package com.example;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+
+    @Mock
+    private Predator mockPredator;
+
+    @Test
+    public void testDoesHaveMane() throws Exception {
+        Lion lion = new Lion("Самец", mockPredator);
+        assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void testGetKittens() throws Exception {
+        when(mockPredator.getKittens()).thenReturn(3);
+
+        Lion lion = new Lion("Самец", mockPredator);
+
+        int kittens = lion.getKittens();
+        assertEquals(3, kittens);
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        when(mockPredator.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        Lion lion = new Lion("Самец", mockPredator);
+
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualFood = lion.getFood();
+        assertEquals(expectedFood, actualFood);
+    }
 }
